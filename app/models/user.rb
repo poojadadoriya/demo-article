286880b74cd76df790ab9f7de0c09fe6195962cd
pooja dadoriya <pooja.dadoriya@taskcraft.in>
2022-09-16 19:08:29 +0530

    mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+
+  # after_create :welcome_email
+
+  def welcome_email
+    UserMailer.welcome_email(self).deliver_now
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,8 +32,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -52,6 +51,7 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.delivery_method = 'smtp'
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
@@ -59,7 +59,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-
- config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => "sonu.rathor@taskcraft.in",
+    :password             => "zjgrrggwdquoxlpa",
+    :authentication       => "plain"
+  }
 end
